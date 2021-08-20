5fb8576d223d4ac9229ff479923d77489ee34c93
TRANDUNG69 <trandungksnb6930082001@gmail.com>
2021-08-20 22:47:21 +0700

    js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-const APP_ID = 'a57c68afe09e9e5fbf2cf9544e375750'
-const DEFAULT_VALUE = "--"
-const city_Name = document.querySelector('.city-name')
-const weather_State = document.querySelector('.weather-state')
-const weather_icon = document.querySelector('.weather-icon')
-const temperature = document.querySelector('.temperature')
-const sunrise = document.querySelector('.sunrise')
-const sunset = document.querySelector('.sunset')
-const humidity = document.querySelector('.humidity')
-const wind_speed = document.querySelector('.wind-speed')
+const APP_ID = 'a57c68afe09e9e5fbf2cf9544e375750';
+// API
+const DEFAULT_VALUE = "--";
+const city_Name = document.querySelector('.city-name');
+const weather_State = document.querySelector('.weather-state');
+const weather_icon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+const humidity = document.querySelector('.humidity');
+const wind_speed = document.querySelector('.wind-speed');
 const search_input = document.querySelector('#search-input');
 const speed = " km/h";
 const degree = "॰";
-const percent = "%"
+const percent = "%";
     search_input.addEventListener('change', (e)=>{
         // console.log(['search_input'], e);
+
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
         .then(async res =>{
             const data = await res.json();
-            console.log('[search_input]', data);
+            // console.log('[search_input]', data);
             city_Name.innerHTML = data.name || DEFAULT_VALUE;
             weather_State.innerHTML = data.weather[0].description || DEFAULT_VALUE;
             weather_icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
